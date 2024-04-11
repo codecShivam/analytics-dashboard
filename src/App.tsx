945b50e3e0a0a58af599e5c59a46ba5be4eca737
codecShivam <codecshivam@gmail.com>
2024-04-11 23:57:12 +0530

    build the sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Sidebar from './components/ui/Sidebar';
 import MetaMaskConnect from "./components/wallet/MetaMaskConnect"
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="flex gap-5 pr-16  max-md:flex-wrap max-md:pr-5">
+      <Sidebar />
       <MetaMaskConnect />
     </div>
   );
---
+++ src/components/ui/Sidebar.tsx
@@ -0,0 +1,77 @@
+import logo from "../../assets/logo.png";
+import credentialsIcon from "../../assets/sidebar/credentials.png";
+import walletIcon from "../../assets/sidebar/walletIcon.png";
+import profileIcon from "../../assets/sidebar/profile.png";
+import graphsIcon from "../../assets/sidebar/profileIcon.png";
+import supportIcon from "../../assets/sidebar/tech support icon.png";
+import repoIcon from "../../assets/sidebar/repo_icon.png";
+import analyticsIcon from "../../assets/sidebar/analytics.png";
+import { useState } from "react";
+
+export default function Sidebar() {
+
+    const [active, setActive] = useState<string>('graphs')
+
+    const handleButtonClick = (button: string) => {
+        setActive(button)
+    }
+
+
+    return (
+        <aside className="flex flex-col items-center px-5 pt-8 h-screen bg-[#0F123F] max-md:pl-5">
+            <button >
+                <img
+                    loading="eager"
+                    src={logo}
+                    alt="Logo"
+                    className="aspect-[1.59] w-[75px]"
+                />
+            </button>
+            <button onClick={() => handleButtonClick('credentials')} className={`${active === 'credentials' ? "rounded-lg bg-gradient-to-br from-[#F57E07] to-[#F8DC7A] " : ""} max-md:mt-2 mt-10 p-[18px]`}>
+                <img
+                    loading="eager"
+                    src={credentialsIcon}
+                    alt="credentials"
+                />
+            </button>
+            <button onClick={() => handleButtonClick('wallets')} className={`${active === 'wallets' ? "rounded-lg bg-gradient-to-br from-[#F57E07] to-[#F8DC7A] " : ""} max-md:mt-2 mt-6 p-[18px]`}>
+                <img
+                    loading="eager"
+                    src={walletIcon}
+                    alt="wallets"
+                />
+            </button>
+            <button onClick={() => handleButtonClick('profile')} className={`${active === 'profile' ? "rounded-lg bg-gradient-to-br from-[#F57E07] to-[#F8DC7A] " : ""} max-md:mt-2 mt-6 p-4 px-[21px]`}>
+                <img
+                    loading="eager"
+                    src={graphsIcon}
+                    alt="profile"
+                />
+            </button>
+            <button onClick={() => handleButtonClick('graphs')} className={`${active === 'graphs' ? "rounded-lg bg-gradient-to-br from-[#F57E07] to-[#F8DC7A] " : ""} max-md:mt-2 mt-6 p-4`}>
+                <img
+                    loading="eager"
+                    src={analyticsIcon}
+                    alt="graphs"
+                    className="border-[3px] rounded-sm p-1"
+                />
+            </button>
+            <div className="flex flex-col  h-full justify-end pb-16">
+                <button onClick={() => handleButtonClick('support')} className={`${active === 'support' ? "rounded-lg bg-gradient-to-br from-[#F57E07] to-[#F8DC7A] " : ""}  mt-6  max-md:mt-2 p-3`}>
+                    <img
+                        loading="eager"
+                        src={supportIcon}
+                        alt="techinical support"
+                    />
+                </button>
+                <button onClick={() => handleButtonClick('repo')} className={`${active === 'repo' ? "rounded-lg bg-gradient-to-br from-[#F57E07] to-[#F8DC7A] " : ""} max-md:mt-2 mt-6 p-[14px]`}>
+                    <img
+                        loading="eager"
+                        src={repoIcon}
+                        alt="repo"
+                    />
+                </button>
+            </div>
+        </aside>
+    );
+}
