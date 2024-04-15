61dd699dd8ed3265677957eded82a6a9ed999d44
codecShivam <codecshivam@gmail.com>
2024-04-15 14:54:18 +0530

    font: DM Sans

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
   };
 
   return (
-    <div className="relative flex">
+    <div className="relative flex font-DM">
       <button
         className={`sm:hidden fixed z-50 top-0 is left-0 mt-4 ml-4 p-2 text-white bg-gray-800 rounded-full`}
         onClick={toggleSidebar}
